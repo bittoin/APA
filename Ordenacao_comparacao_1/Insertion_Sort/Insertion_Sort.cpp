13035c3ca0a4da926f6f624da8d2bccaf90f3582
Mateus <mateustoin@gmail.com>
2018-07-24 15:27:22 -0300

    Codigo atualizado com casos de teste e tempo de execução do algoritmo

--- Ordenacao_comparacao_1/Insertion_Sort/Insertion_Sort.cpp
+++ Ordenacao_comparacao_1/Insertion_Sort/Insertion_Sort.cpp
@@ -7,37 +7,42 @@
 
 #include <iostream>
 #include <vector>
-#include <string>
 #include <utility> //Contém função swap
+#include <chrono>
 
 using namespace std;
 
 int main(){
-    vector<int> item;
-    cout << "Insira os elementos da lista. (Para com a inserção do caractere 'n')" << endl;
-    //Entrada de dados
-    for (int i = 0;; i++){
-        cout << "Item " << i << ": ";
-        string tempItem;
-        cin >> tempItem;
-        if (!tempItem.compare("n")) break;
-        item.push_back(stoi(tempItem));
-    }
+    vector<int> item = {5, 4, 90, -3, 70, 18, 0, 1, -400, 900, 25, 43, 51, 13, -6, -14, 200, 151, -9, 0, 45, 1045, 0, -553}; /* Caso médio */
+    //vector<int> item = {1045, 900, 200, 151, 90, 70, 51, 45, 43, 25, 18, 13, 5, 4, 1, 0, 0, 0, -3, -6, -9, 14, -400, -553}; /* Pior caso */
+    //vector<int> item = {-553, -400, -14, -9, -6, -3, 0, 0, 0, 1, 4, 5, 13, 18, 25, 43, 45, 51, 70, 90, 151, 200, 900, 1045}; /* Melhor caso */
+    
+    int pivoIndex = 0;
+    
+    // Começa a contagem do tempo
+    auto start = chrono::high_resolution_clock::now();
     
     //Ordenação dos dados
     for(unsigned int i = 0; i < item.size()-1; i++){
-        int pivoIndex = i+1;
+        pivoIndex = i+1;
         while(pivoIndex > 0 && (item[pivoIndex] < item[pivoIndex-1])){
             swap(item[pivoIndex], item[pivoIndex-1]);
             pivoIndex--;
         }
     }
+    
+    // Armazena o fim do tempo
+    auto finish = chrono::high_resolution_clock::now();
+    // Realiza a diferença do tempo entre o final e o início do algoritmo
+    chrono::duration<double> elapsed = finish - start;
 
     //Visualização dos dados ordenados
     cout << endl << "Elementos ordenados: " << endl;
     for(unsigned int i = 0; i < item.size(); i++){
         cout << "Item[" << i << "]: " << item[i] << endl;
     }
+    
+    cout << "Tempo de execução do código: " << elapsed.count() << " s\n";
 
     return 0;
 }
