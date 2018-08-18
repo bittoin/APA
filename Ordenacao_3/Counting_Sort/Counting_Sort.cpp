ce3f5c516a6bf9bb84719e3965e1fec0939b7367
Mateus Antonio <mateustoin@gmail.com>
2018-08-18 11:47:27 -0300

    Counting sorte para numeros positivos operando sem erros

--- Ordenacao_3/Counting_Sort/Counting_Sort.cpp
+++ Ordenacao_3/Counting_Sort/Counting_Sort.cpp
@@ -17,7 +17,7 @@ void exibeItens(vector<int> &item){
     }
 }
 
-void countingSort(vector<int> &item){
+void countingSort(vector<int> &item, vector<int> &novoArray){
     int maiorNum = 0;
     for (unsigned int i = 0; i < item.size(); i++){
         if (item[i] > maiorNum)
@@ -25,39 +25,40 @@ void countingSort(vector<int> &item){
     }
 
     //Criando array "C" com a quantidade de indices referente ao maior número encontrado no vector
-    vector<int> itemAux(maiorNum);
+    vector<int> itemAux(maiorNum + 1);
 
     //Usa os valores do vector principal para contar a quantidade de vezes aquele número foi encontrado no seu respectivo índice
     for (unsigned int i = 0; i < item.size(); i++){
         itemAux[item[i]]++;
     }
-    cout << "Vamos testar uma posição: " << itemAux[5] << endl;
+        //cout << "Vamos testar uma posição: " << itemAux[5] << endl;
+    
     //Realiza soma da contagem em relação a todos os elementos anteriores ao atual
     for (unsigned int i = 1; i <= itemAux.size(); i++){
         itemAux[i] += itemAux[i-1];
     }
-
-    exibeItens(itemAux);
-    
+ 
     //Criado para representar o vector "B" que armazena os valores ordenados
-    vector<int> novoArray(item.size()); 
+    //vector<int> novoArray(item.size()); 
 
     //Atribui valores ordenados no vector "B" secundário
-    for (unsigned int i = item.size(); i > 0; i--){
+    for (int i = item.size(); i >= 0; --i){
         novoArray[itemAux[item[i]] - 1] = item[i];
         itemAux[item[i]]--;
     }
 
-    novoArray.clear();
-
-    exibeItens(novoArray);
+    itemAux.clear();
 }
 
 int main(){
     vector<int> item = {5, 4, 90, 70, 18, 0, 1, 900, 25, 43, 51, 13, 200, 151, 0, 45, 1045, 0}; /* Apenas valores positivos */
     //vector<int> item = {5, 4, 90, -3, 70, 18, 0, 1, -400, 900, 25, 43, 51, 13, -6, -14, 200, 151, -9, 0, 45, 1045, 0, -553}; /* Caso com negativos */
+    //vector<int> item = {5, 1, 0, 10, 3, 0, 7, 2}; /* Apenas valores positivos */
+
+    vector<int> novoArray(item.size()); //Armazena valores ordenados
     
-    countingSort(item);
+    countingSort(item, novoArray);
+    exibeItens(novoArray);
 
     return 0;
 }
