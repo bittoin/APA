b2524e0e226b31fe5b1e4e2f6ae200642d02775b
Mateus Antonio <mateustoin@gmail.com>
2018-08-18 11:51:49 -0300

    Pequeno ajuste

--- Ordenacao_3/Counting_Sort/Counting_Sort.cpp
+++ Ordenacao_3/Counting_Sort/Counting_Sort.cpp
@@ -55,10 +55,10 @@ int main(){
     //vector<int> item = {5, 4, 90, -3, 70, 18, 0, 1, -400, 900, 25, 43, 51, 13, -6, -14, 200, 151, -9, 0, 45, 1045, 0, -553}; /* Caso com negativos */
     //vector<int> item = {5, 1, 0, 10, 3, 0, 7, 2}; /* Apenas valores positivos */
 
-    vector<int> novoArray(item.size()); //Armazena valores ordenados
+    vector<int> vectorOrdenado(item.size()); //Armazena valores ordenados
     
-    countingSort(item, novoArray);
-    exibeItens(novoArray);
+    countingSort(item, vectorOrdenado);
+    exibeItens(vectorOrdenado);
 
     return 0;
 }
