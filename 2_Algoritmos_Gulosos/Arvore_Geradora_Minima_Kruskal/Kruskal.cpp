44a500381948b98d5976969ce6183d40d7fdd40c
Mateus Antonio <mateustoin@gmail.com>
2018-09-27 14:56:01 -0300

    Correção feita no algoritmo

--- 2_Algoritmos_Gulosos/Arvore_Geradora_Minima_Kruskal/Kruskal.cpp
+++ 2_Algoritmos_Gulosos/Arvore_Geradora_Minima_Kruskal/Kruskal.cpp
@@ -36,7 +36,6 @@ int main()
         }
     }
 
-
     //INSERTION SORT PARA ORDENAR AS ARESTAS DE ACORDO COM SEU PESO (IMPLEMENTADO NA PRIMEIRA ENTREGA)
     int pivoIndex = 0;
     for(unsigned int i = 0; i < aresta.size()-1; i++){
@@ -47,46 +46,48 @@ int main()
         }
     }
 
-    /*for (int i = 0; i < aresta.size(); i++){
+    for (unsigned int i = 0; i < aresta.size(); i++){
         cout << i << " >> " << "[" << aresta[i].v1 << "]" << "[" << aresta[i].v2 << "]" << " = " << aresta[i].peso << endl;
-    }*/
+    }
 
     //CRIA VECTOR COM IDENTIFICADORES PARA AS ÁRVORES
     vector<int> arvoreID(tam_matriz);
     for (int i = 0; i < tam_matriz; i++){
-        arvoreID[i] = -1;
+        arvoreID[i] = i;
     }
 
     //Cria vector de arestas onde serão armazenados os vértices da arvore geradora mínima
     vector<Aresta> MST;
 
-    /*for (int i = 0; i < aresta.size(); i++){
+    for (unsigned int i = 0; i < aresta.size(); i++){
         if (arvoreID[aresta[i].v1] != arvoreID[aresta[i].v2]){
-
+            //arvoreID[aresta[i].v2] = arvoreID[aresta[i].v1];
+            
             for (int j = 0; j < tam_matriz; j++){
-                if (arvoreID[j] == aresta[i].v1)
-                    arvoreID[j] = aresta[i].v2;
+                if (arvoreID[j] == aresta[i].v2)
+                    arvoreID[j] = arvoreID[aresta[i].v1];
             }
-
             MST.push_back(aresta[i]);
 
-            if (MST.size() >= tam_matriz - 1)
+            if (MST.size() >= tam_matriz -1)
                 break;
         }
-    }*/
+    }
 
-    for (unsigned int i = 0; i < aresta.size(); i++){
+    /*for (unsigned int i = 0; i < aresta.size(); i++){
         if (arvoreID[aresta[i].v2] == -1){
             MST.push_back(aresta[i]);
             arvoreID[aresta[i].v2] = aresta[i].v2;
         }
-    }
+    }*/
 
+    int soma = 0;
     //IMPRIME NA TELA O VECTOR DE ARESTAS MST
     for (unsigned int i = 0; i < MST.size(); i++){
         cout << i << " >> " << "[" << MST[i].v1 << "]" << "[" << MST[i].v2 << "]" << " = " << MST[i].peso << endl;
-        somaPesos += MST[i].peso;
+        soma += MST[i].peso;
     }
+    cout << "SOMA: " << soma << endl;
 
     return 0;
 }
